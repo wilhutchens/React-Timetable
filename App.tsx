12fd2b944d20184e14ad06f438cd7181433436d6
wilhutchens <33008564+wilhutchens@users.noreply.github.com>
2023-02-05 20:32:15 +0000

    4 files modified

--- App.tsx
+++ App.tsx
@@ -9,3 +9,24 @@ export default function App() {
     </div>
   );
 }
+
+export default function App() {
+  return (
+    <div style={{ backgroundColor: '#xxxx00' }}>
+      <div className="toolbar">
+        <div className="title">RNLI</div>
+        <div className="nav">
+          <div className="nav-item">Timetable</div>
+          <div className="nav-item">Lifeguards</div>
+          <div className="nav-item">Beaches</div>
+          <div className="nav-item">Profile</div>
+          <div className="nav-item">Contact</div>
+        </div>
+      </div>
+      <div>
+      <Timetable />
+    </div>
+    </div>
+  );
+}
+
